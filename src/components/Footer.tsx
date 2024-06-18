3a85a431c4b8dfa206eab119b2a0c3e9afd75f40
Andrés Tavares Tena <82838112+Andy-TAbs@users.noreply.github.com>
2024-06-18 10:00:08 -0600

    Update header.tsx Footer.tsx and page.tsx, add  resources to public

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,14 +1,16 @@
 import React from 'react';
 
-interface HeaderProps {
-    title: string;
-}
-const title = "Kalisch Acero";
 
-export const Footer: React.FC<HeaderProps> = ({ title }) => {
+export interface FooterProps {
+    // Define any props for the Footer component here
+}
+
+export const Footer: React.FC<FooterProps> = () => {
     return (
-        <footer className="flex flex-col md:flex-row items-center justify-between">
-            {/*Hola*/}
+        <footer className="flex flex-col md:flex-row items-center justify-between w-screen m-20">
+            <div>
+                <p>gf</p>
+            </div>
         </footer>
     );
 };
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/no-img-element */
 import React from 'react';
 
 interface HeaderProps {
@@ -7,15 +8,26 @@ const title = "Kalisch Acero";
 
 export const Header: React.FC<HeaderProps> = ({ title }) => {
     return (
-        <header className="flex flex-col md:flex-row items-center justify-between">
-            <h1 className="text-2xl md:text-4xl">{title}</h1>
-            <nav className="mt-4 md:mt-0">
-                <ul className="flex space-x-4">
-                    <li>Home</li>
-                    <li>About</li>
-                    <li>Contact</li>
-                </ul>
-            </nav>
-        </header>
+        <div className="w-screen justify-start m-20">
+            <header className='justify-end items-end'>
+                <div className="">
+                    <p>hola</p>
+                </div>
+            </header>
+            <header className="flex-column">
+                <div className='justify-start'>
+                    <img src="/kfa_movil_50.png" alt="logo_KalischAcero" className="w-48 h-auto items-start" />
+                </div>
+                <div className=''>
+                    <nav className="mt-4 md:mt-0">
+                        <ul className="flex space-x-4">
+                            <li>Home</li>
+                            <li>About</li>
+                            <li>Contact</li>
+                        </ul>
+                    </nav>
+                </div>
+            </header>
+        </div>
     );
 };
