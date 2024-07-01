d82054053519bf06ce2db5b09c2395cd8baa4632
Andrés Tavares Tena <82838112+Andy-TAbs@users.noreply.github.com>
2024-07-01 09:12:31 -0600

    Fix Responsive design in Headers

--- src/components/Header/Global/Header.tsx
+++ src/components/Header/Global/Header.tsx
@@ -15,18 +15,18 @@ export const Header: React.FC<HeaderProps> = ({ title }) => {
             <div className="hidden lg:block h-10">
                 <SecHeader title="Header" />
             </div>
-            <div className="h-20">
+            <div className="h-28">
                 <header className="w-full">
                     <nav className="bg-white border-gray-200 px-2 lg:px-4 py-2.5 dark:bg-gray-800">
                         <div className="flex items-center justify-between w-full lg:justify-start lg:flex-grow-0 lg:order-2">
                             <div className="lg:hidden">
                                 <DropdownMenuMobile />
                             </div>
-                            <a href="https://flowbite.com" className="mx-auto lg:mx-0 lg:ml-12">
-                                <img src="/kfa_movil_50.png" className="h-8 sm:h-9 lg:ml-0 xl:ml-0  2xl:ml-36" alt="Kalisch_Logo"/>
+                            <a href="https://flowbite.com" className="mx-auto lg:mx-0 xl::ml-12">
+                                <img src="/kfa_movil_50.png" className="lg:h-9 sm:h-9 2xl:pl-20 2xl:ml-28" alt="Kalisch_Logo"/>
                             </a>
                             <img src="/search.png" className="w-6 h-6 lg:hidden" alt="Search" />
-                            <div className="hidden lg:flex flex-col lg:flex-row lg:items-center lg:ml-auto xl:mr-48 ">
+                            <div className="hidden lg:flex flex-col lg:flex-row lg:items-center lg:ml-auto 2xl:mr-52 ">
                                 <ul className="flex flex-wrap list-none justify-center lg:justify-start lg:space-x-4 space-y-4 lg:space-y-0 ">
                                 <li className="">
                                     <button className="block w-24 h-14 mt-2  bg-green-700 text-white text-sm border-green-700 border-b transition-colors duration-300 ease-in-out hover:bg-green-500 lg:border-0 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700">
