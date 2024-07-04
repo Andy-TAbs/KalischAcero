6e3466e39c5f7b8584a0a32b77be0d6bb4106776
Andrés Tavares Tena <82838112+Andy-TAbs@users.noreply.github.com>
2024-07-04 10:46:07 -0600

    Update size in heights and withs in tailwind.config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,7 +9,20 @@ const config: Config = {
   theme: {
     extend: {
       screens: {
-        'xs': '300px',  
+        'xs': '300px',
+      },
+      spacing: {
+        '41': '10.25rem',
+        '42': '10.5rem',
+        '43': '10.75rem',
+        '104': '26rem',
+        '112': '28rem',
+        '116': '29rem',
+        '120': '30rem',
+        '128': '32rem',
+        '144': '36rem',
+        '160': '40rem',
+        '192': '48rem',
       },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
