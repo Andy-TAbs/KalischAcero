bfa641be7a35ec0b44561771d64caf306c694a2f
Andrés Tavares Tena <82838112+Andy-TAbs@users.noreply.github.com>
2024-07-11 09:21:11 -0600

    Fix FlipCard tex orientation

--- src/components/Global/FlipCard.tsx
+++ src/components/Global/FlipCard.tsx
@@ -27,10 +27,12 @@ const FlipCard = ({ image, title, text, link }: FlipCardProps) => {
             <div className={`absolute w-full h-full transition-transform transform ${flipped ? 'rotate-y-180' : ''}`}>
                 <div className="w-full h-full bg-white rounded shadow-md border border-gray-200 flex items-center justify-center backface-hidden">
                     {flipped ? (
-                        <div className="text-center backface-hidden transform rotate-y-180">
-                            <p className="text-lg font-semibold mb-4">{title}</p>
+                        <div className="text-center backface-hidden transform ">
+                            <p className="text-lg font-semibold mb-4 rotate-y-180">{title}</p>
                             <p className="transform rotate-y-180">{text}</p>
-                            <a href={link} className="text-blue-500 hover:underline transform rotate-y-180">Enlace</a>
+                            <div className='rotate-y-180'>
+                                <a href={link} className="text-blue-500  hover:underline transform">Enlace</a>
+                            </div>
                         </div>
                     ) : (
                         <img src={image} alt="Imagen frontal" className="w-full h-full object-cover rounded backface-hidden" />
