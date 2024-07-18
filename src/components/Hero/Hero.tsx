010fb87dc72f91d8f99f98066edc0f0cc9d75a53
Andrés Tavares Tena <82838112+Andy-TAbs@users.noreply.github.com>
2024-07-18 11:51:04 -0600

    Modify Globals.css to add Bitton Top style

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -84,12 +84,22 @@ export const Hero = () => {
                         <img src="/Logotipo_KA2022_v19.png" className="absolute top-60 left-80 h-44 w-112 max-w-full max-h-full ml-8" alt="Logotipo_KA2022_v19" />
                     </div>
                 </div>
+                <div className="fixed z-50 items-end bottom-5 right-8">
+                    <div className="sticky-position xs:hidden md:block">
+                        <button className="button" title="Button">
+                            <svg className="svgIcon" viewBox="0 0 384 512">
+                                <path
+                                    d="M214.6 41.4c-12.5-12.5-32.8-12.5-45.3 0l-160 160c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L160 141.2V448c0 17.7 14.3 32 32 32s32-14.3 32-32V141.2L329.4 246.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3l-160-160z"
+                                ></path>
+                            </svg>
+                        </button>
+                    </div>
+                </div>
             </div>
             <div className="w-full">
                 <Carousel />
             </div>
             {/* Seccion de Años*/}
-            
             <div className="xl:h-80 lg:h-auto bg-slate-900 xs:py-16 items-center justify-center lg:flex lg:flex-row">
                 {showCounters && (
                     <>
