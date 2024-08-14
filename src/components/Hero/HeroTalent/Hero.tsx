dcb4fa13754b6218f16ca5945d5f5a9299a04897
Andrés Tavares Tena <82838112+Andy-TAbs@users.noreply.github.com>
2024-08-14 13:52:20 -0600

    Update: added style in bg with videos and titles

--- src/components/Hero/HeroTalent/Hero.tsx
+++ src/components/Hero/HeroTalent/Hero.tsx
@@ -25,7 +25,21 @@ const accordionItems = [
 const Hero: React.FC<HeroProps> = () => {
     return (
         <div className="w-screen items-center">
-            <div className='bg-black h-204'></div>
+            <div className='h-204 relative '>
+                <video autoPlay muted loop id="background-video" className="absolute inset-0 z-0 w-full h-full object-cover">
+                    <source src="/nosotrostitulos.mp4" type="video/mp4" />
+                    Tu navegador no soporta el video.
+                </video>
+                <div className='absolute inset-0 flex lg:top-56 md:top-56 sm:top-56 xs:top-56 xsm:top-56 lg:left-42 md:left-32 mdl:left-48 sm:left-36 xsm:left-28 xs:left-10 z-10'>
+                    <p className='sm:text-sm xs:text-xxs font-Bebas_Neue text-white'>I N I C I O  /  N O S O T R O S</p>
+                </div>
+                <div className='absolute inset-0 flex lg:top-56 md:top-56 sm:top-60 xs:top-60 xsm:top-60 lg:left-42 md:left-32 mdl:left-48 sm:left-36 xsm:left-28 xs:left-10 z-10'>
+                    <p className='md:text-11xl xs:text-7xl sm:text-9xl font-Bebas_Neue text-white font-extrabold'>ACERCA DE</p>
+                </div>
+                <div className='absolute inset-0 flex lg:top-88 md:top-88 sm:top-88 xs:top-76 xsm:top-76 lg:left-42 md:left-32 mdl:left-48 sm:left-36 xsm:left-28 xs:left-10  z-10'>
+                    <p className='md:text-11xl xs:text-7xl sm:text-9xl font-Bebas_Neue text-white font-extrabold'>NOSOTROS</p>
+                </div>
+            </div>
             <div className='z-50'>
                 <SocialButtons />
             </div>
