7a6df560309646fc5e6fe9325195da457370cc79
Andrés Tavares Tena <82838112+Andy-TAbs@users.noreply.github.com>
2024-09-02 13:15:13 -0600

    Add Hero Components in Sostenibilidad Section

---
+++ src/components/Hero/SostenibilidadPages/HeroPoliticaMA/Hero.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+interface Props {
+    // Define las propiedades del componente aquí
+}
+
+const Hero: React.FC<Props> = () => {
+    return (
+        <div>
+            {/* Contenido del componente */}
+        </div>
+    );
+};
+
+export default Hero;
