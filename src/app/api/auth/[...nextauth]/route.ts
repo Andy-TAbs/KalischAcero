f079336bf78128d5197567180950690dc47ff447
Andrés Tavares Tena <82838112+Andy-TAbs@users.noreply.github.com>
2024-09-11 08:28:18 -0600

    Update nexauth route

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -2,6 +2,7 @@ import NextAuth from "next-auth/next";
 import GitHubProvider from "next-auth/providers/github";
 
 const handler = NextAuth({
+    secret: process.env.NEXTAUTH_SECRET as string,
     providers: [
         GitHubProvider({
             clientId: process.env.GITHUB_ID as string,
