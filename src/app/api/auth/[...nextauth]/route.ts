ee50084273c8f5b468e7f6640db4cd279386ff1c
Andrés Tavares Tena <82838112+Andy-TAbs@users.noreply.github.com>
2024-09-23 09:41:27 -0600

    Fix: deploy in route.ts

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -3,43 +3,11 @@ import GitHubProvider from "next-auth/providers/github";
 import { MongoDBAdapter } from "@auth/mongodb-adapter";
 import client from "../../../../../lib/db";
 import { Adapter } from "next-auth/adapters";
-import Credentials from "next-auth/providers/credentials";
-import { signInSchema } from "../../../../../lib/zod/zod";
-import { ZodError } from "zod";
-import { saltAndHashPassword } from "@/utils/SaltHashedPassword";
 
 const handler = NextAuth({
     adapter: MongoDBAdapter(client) as Adapter,
     secret: process.env.NEXTAUTH_SECRET as string,
     providers: [
-        Credentials({
-            // You can specify which fields should be submitted, by adding keys to the `credentials` object.
-            // e.g. domain, username, password, 2FA token, etc.
-            credentials: {
-                email: {},
-                password: {},
-            },
-            authorize: async (credentials) => {
-                try {
-                    let user = null
-                    const { email, password } = await signInSchema.parseAsync(credentials)
-                // logic to salt and hash password
-                    const pwHash = saltAndHashPassword(password)
-                // logic to verify if the user exists
-                    user = await getUserFromdb(email, pwHash)
-                    if (!user) {
-                        throw new Error("User not found.")
-                }
-                // return JSON object with the user data
-                    return user
-                } catch (error) {
-                    if (error instanceof ZodError) {
-                  // Return `null` to indicate that the credentials are invalid
-                    return null
-                    }
-                }
-            },
-        }),
         GitHubProvider({
             clientId: process.env.GITHUB_ID as string,
             clientSecret: process.env.GITHUB_SECRET as string,
