39fbfb51ccfaf4b64df7512d568a0520da1e8a75
Andrés Tavares Tena <82838112+Andy-TAbs@users.noreply.github.com>
2024-10-02 12:57:00 -0600

    Update dashboard News page

--- src/app/dashboard/Nosotros/page.tsx
+++ src/app/dashboard/Nosotros/page.tsx
@@ -1,5 +1,5 @@
 import { getServerSession } from "next-auth"
-import NosotrosHeroDashboard from "@/components/Dashboard/HeroDashboard/NosotrosHeroDashboard";
+import NosotrosHeroDashboard from "@/components/Dashboard/HeroDashboard/Nosotros/NosotrosHeroDashboard";
 import HeaderDashboard from "@/components/Dashboard/HeaderDashboard";
 import SideMenuDashboard from "@/components/Dashboard/SideMenuDashboard";
 
