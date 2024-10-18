a3c164ac98a7a3a02c6d62e886162472831375fb
Andrés Tavares Tena <82838112+Andy-TAbs@users.noreply.github.com>
2024-10-18 10:25:29 -0600

    Update nextauth route to support mongodb conn

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,11 +1,23 @@
-import NextAuth from "next-auth/next";
+import NextAuth, { Session } from "next-auth";
 import GitHubProvider from "next-auth/providers/github";
 import { MongoDBAdapter } from "@auth/mongodb-adapter";
 import client from "../../../../../lib/db";
-import { Adapter } from "next-auth/adapters";
+
+declare module "next-auth" {
+    interface Session {
+        user: {
+            id: string;
+            name?: string | null;
+            email?: string | null;
+            image?: string | null;
+        };
+        accessToken?: string;
+    }
+}
 
 const handler = NextAuth({
     secret: process.env.NEXTAUTH_SECRET as string,
+    adapter: MongoDBAdapter(client), // Adaptador para guardar datos de la cuenta en MongoDB
     providers: [
         GitHubProvider({
             clientId: process.env.GITHUB_ID as string,
@@ -15,6 +27,34 @@ const handler = NextAuth({
     pages: {
         signIn: "/Iniciar-Sesion",
     },
-})
+    session: {
+        strategy: "jwt", // Usar JWT para la gestión de la sesión
+    },
+    callbacks: {
+        async jwt({ token, user, account }) {
+            // Almacena información relevante en el token JWT
+            if (user) {
+                token.id = user.id;
+                token.name = user.name;
+                token.email = user.email;
+            }
+            if (account) {
+                token.accessToken = account.access_token;
+            }
+            return token;
+        },
+        async session({ session, token }) {
+            // Pasar los datos del token a la sesión
+            if (session.user) {
+                session.user.id = token.id as string;
+                session.user.name = token.name as string;
+                session.user.email = token.email as string;
+                session.accessToken = token.accessToken as string;
+            }
+            return session;
+        },
+    },
+    debug: true, // Activar para depuración
+});
 
-export { handler as GET, handler as POST }
+export { handler as GET, handler as POST };
